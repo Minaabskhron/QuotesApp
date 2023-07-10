b8a6786c1ac5446ffd6241917a39862f9874d5ed
minaabskhron <minaabskhron@gmail.com>
2023-07-10 21:26:31 +0300

    initial commit

---
+++ js/main.js
@@ -0,0 +1,38 @@
+var Quotes=[
+    {
+        quote:"“Be yourself; everyone else is already taken.”",
+        author:"― Oscar Wilde"
+    },
+
+    {
+        quote:"“Two things are infinite: the universe and human stupidity; and I'm not sure about the universe.”",
+        author:"― Albert Einstein"
+    },
+
+    {
+        quote:"“So many books, so little time.”",
+        author:"― Frank Zappa"
+    },
+
+    {
+        quote:"“A room without books is like a body without a soul.”",
+        author:"― Marcus Tullius Cicero"
+    },
+
+    {
+        quote:"“Be who you are and say what you feel, because those who mind don't matter, and those who matter don't mind.”",
+        author:"― Bernard M. Baruch"
+    },
+
+    {
+        quote:"“You only live once, but if you do it right, once is enough.”",
+        author:"― Mae West"
+    },
+]
+//Math.trunc(Math.random()*7+1)
+function getQuotes()
+{
+    var randomIndex=Math.trunc(Math.random()*6);
+    document.getElementById("theQuote").innerHTML=Quotes[randomIndex].quote;
+    document.getElementById("theAuthor").innerHTML=Quotes[randomIndex].author;
+}
